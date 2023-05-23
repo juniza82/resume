823d91c80881dabbf1992d649607006c3f753486
buzz.kim <buzz.kim@allstay.com>
2023-05-23 23:00:15 +0900

    Initial commit

---
+++ src/main/java/com/juniza82/resume/controller/TestController.java
@@ -0,0 +1,24 @@
+package com.juniza82.resume.controller;
+
+import com.juniza82.resume.domain.TestResponseDomain;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/hello")
+    public String Hello() {
+        System.out.println("Hello!!");
+        return "hello";
+    }
+
+    @GetMapping("/hello/domain")
+    public TestResponseDomain hellDomain(@RequestParam("name") String name, @RequestParam("amount") int amount) {
+        System.out.println("/hello/domain!!");
+
+        return new TestResponseDomain(name, amount);
+    }
+
+}
---
+++ src/main/java/com/juniza82/resume/domain/TestResponseDomain.java
@@ -0,0 +1,14 @@
+package com.juniza82.resume.domain;
+
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+
+@RequiredArgsConstructor
+public class TestResponseDomain {
+
+    private final String name;
+    private final int amount;
+
+}
